60766137516a1929d5d18f91df4938dd2071f9a0
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-22 23:39:46 +0700

    menaikan ndkversion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 android {
     namespace = "com.example.plantapp"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    android {
+    ndkVersion = "27.0.12077973"
+}
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
