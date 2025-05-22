c0ae05eb8abc9d02eab49af7a007a5256007f648
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-23 00:18:33 +0700

    home screen plantapp

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/components/my_bottom_nav_bar.dart';
+import 'package:plantapp/home/components/body.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+      bottomNavigationBar: MyBottomNavBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Color(0xFF0C9869),
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
