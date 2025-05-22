0e69f87dc4dae098bc90ddcaea09b6fd5835c008
WisnuDwiSeisno <wseisno37@gmail.com>
2025-05-23 00:26:05 +0700

    inisialisasi MapPage dengan setup lokasi awal pengguna

---
+++ lib/maps/map_page.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _currentAddress;
+  String? _pickedAddress;
+  CameraPosition? _initialCamera;
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupLocation();
+  }
+
+  Future<void> _setupLocation() async {
+    try {
+      final pos = await getPermissions();
+      _currentPosition = pos;
+      _initialCamera = CameraPosition(
+        target: LatLng(pos.latitude, pos.longitude),
+        zoom: 16,
+      );
+      final placemarks = await placemarkFromCoordinates(
+        _currentPosition!.latitude,
+        _currentPosition!.longitude,
+      );
+
+      final p = placemarks.first;
+      _currentAddress = '${p.name}, ${p.locality}, ${p.country}';
+
+      setState(() {});
+    } catch (e) {
+      _initialCamera = const CameraPosition(target: LatLng(0, 0), zoom: 2);
+      setState(() {});
+      print(e);
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  Future<Position> getPermissions() async {
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      throw ('Location service belum aktif');
+    }
+
+    LocationPermission perm = await Geolocator.checkPermission();
+    if (perm == LocationPermission.denied) {
+      perm = await Geolocator.requestPermission();
+      if (perm == LocationPermission.denied) {
+        throw ('Permission ditolak');
+      }
+    }
+    if (perm == LocationPermission.deniedForever) {
+      throw ('Permission ditolak selamanya');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+}
